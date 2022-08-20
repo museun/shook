65e0121a0df27220986d92a798c2f5eff8146885
museun <museun@outlook.com>
2022-08-20 10:51:54 -0400

    rename crate, make file loadable from env

--- shook_brain_serve/src/main.rs
+++ shook_brain_serve/src/main.rs
@@ -2,7 +2,7 @@ use std::path::{Path, PathBuf};
 
 use gumdrop::Options;
 
-use shook_brain::{start_server, ManagedBrain, Messaging, GENERATE_TIMEOUT, SAVE_DURATION};
+use shook_brain_serve::{start_server, ManagedBrain, Messaging, GENERATE_TIMEOUT, SAVE_DURATION};
 
 #[derive(Debug, Options)]
 /// serves shaken's brains for the internet to consume
@@ -11,19 +11,23 @@ struct Config {
     help: bool,
 
     /// path of brain to use
-    #[options(required, meta = "<path>")]
-    file: PathBuf,
+    #[options(meta = "<path>")]
+    file: Option<PathBuf>,
 
     /// address to listen on
-    #[options(default = "localhost:50000", meta = "<addr>")]
+    #[options(default = "localhost:8000", meta = "<addr>")]
     address: String,
 }
 
 async fn load(path: &Path) -> anyhow::Result<Messaging> {
-    let brain = shook_brain::load(path).await?;
+    let brain = shook_brain_serve::load(path).await?;
     log::trace!("spawning brain handle thread");
     let msg = ManagedBrain::spawn(brain, path, GENERATE_TIMEOUT, SAVE_DURATION);
     Ok(Messaging::new(msg))
+}
+
+fn get_env_var(key: &str) -> anyhow::Result<String> {
+    std::env::var(key).or_else(|_| anyhow::bail!("cannot find env var for '{key}'"))
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -37,10 +41,15 @@ async fn main() -> anyhow::Result<()> {
     .init()?;
 
     let config = Config::parse_args_default_or_exit();
-    let bearer = std::env::var("SHAKEN_BRAIN_SUPER_SECRET_BEARER_TOKEN")?;
+    let bearer = get_env_var("SHOOK_BRAIN_SUPER_SECRET_BEARER_TOKEN")?;
 
-    log::info!("loading brain from {}", config.file.display());
-    let brain = load(&config.file).await?;
+    let file = match config.file {
+        Some(path) => path,
+        None => get_env_var("SHOOK_BRAIN_FILE").map(PathBuf::from)?,
+    };
+
+    log::info!("loading brain from {}", file.display());
+    let brain = load(&file).await?;
     log::debug!("loaded brain");
 
     start_server(config.address, brain, &bearer).await
